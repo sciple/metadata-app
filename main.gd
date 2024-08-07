670c20f4e30166b1fa59c3ac61ba42200448a60d
Leonardo Restivo <leonardo.restivo@gmail.com>
2024-08-07 11:50:44 +0200

    save/load function (broken)

--- main.gd
+++ main.gd
@@ -45,6 +45,55 @@ func _on_graph_edit_connection_request(from_node, from_port, to_node, to_port):
 func _on_graph_edit_disconnection_request(from_node, from_port, to_node, to_port):
 	$GraphEdit.disconnect_node(from_node, from_port, to_node, to_port)
 
+func save_data(file_name):
+	var graph_data = GraphData.new()
+	graph_data.connections = Global.main_graph.get_connection_list()
+	for node in Global.main_graph.get_children():
+		if node is GraphNode:
+			print(node)
+			var node_data = NodeData.new()
+			node_data.name = node.name
+			#node_data.type = node.type
+			node_data.position_offset = node.position_offset
+			#node_data.data = node.data
+			graph_data.nodes.append(node_data)
+	if ResourceSaver.save(graph_data, file_name) == OK:
+		print("saved")
+	else:
+		print("Error saving graph_data")
+
+
+func load_data("save.res"):
+	if ResourceLoader.exists("save.res"):
+		var graph_data = ResourceLoader.load(file_name)
+		if graph_data is GraphData:
+			init_graph(graph_data)
+		else:
+			# Error loading data
+			pass
+	else:
+		# File not found
+		pass
+
+
+func init_graph(graph_data: GraphData):
+	clear_graph()
+	for node in graph_data.nodes:
+		# Get new node from factory autoload (singleton)
+		var gnode = PartFactory.get_node(node.type)
+		gnode.offset = node.offset
+		gnode.name = node.name
+		get_node("Graph").add_child(gnode)
+	for con in graph_data.connections:
+		var _e = get_node("Graph").connect_node(con.from, con.from_port, con.to, con.to_port)
+
+func clear_graph():
+	get_node("Graph").clear_connections()
+	var nodes = get_node("Graph").get_children()
+	for node in nodes:
+		if node is GraphNode:
+			node.queue_free()
+
 
 func save():
 	#Global.init_subjects()
@@ -89,7 +138,7 @@ func update_global_stats():
 	$cage_count.text = ", " + str(Global.cages.size())
 
 func _on_save_button_pressed():
-	save()
+	save_data("save.res")
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_released() and event.button_index == MOUSE_BUTTON_RIGHT:
---
+++ node_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name NodeData
+
+@export var name: String
+@export var type: String
+@export var position_offset: Vector2
+@export var data = {}
